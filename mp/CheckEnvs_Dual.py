a5cf0a653781c827c076757fb41522d05b4a0abc
eaooms <90698566+eaooms@users.noreply.github.com>
2024-04-23 16:14:47 +0200

    Update CheckEnvs_Dual.py

--- mp/CheckEnvs_Dual.py
+++ mp/CheckEnvs_Dual.py
@@ -21,7 +21,7 @@ major_contigs_order = ["chr" + str(a) for a in list(range(1, 23)) + ["X", "Y"]] 
                                                                                    list(range(1, 23)) + ["X", "Y"]]
 
 required_tool_version = {
-    'python': LooseVersion('3.6.18'),
+    'python': LooseVersion('3.6.10'),
     'pypy': LooseVersion('3.6'),
     'samtools': LooseVersion('1.15.1'),
     'whatshap': LooseVersion('1.7'),
